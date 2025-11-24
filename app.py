e8ed5be16737ce4952b5f2a4ca2a19a623cb6fae
yiruwu77 <yiruwu77@gmail.com>
2025-11-24 18:17:41 +0000

    1

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    r = request.form.get("q")
     return(render_template("index.html"))
 
 @app.route("/main", methods=["GET","POST"])
 def main():
+    r = request.form.get("q")
     return(render_template("main.html"))
 
 @app.route("/dbs", methods=["GET","POST"])
